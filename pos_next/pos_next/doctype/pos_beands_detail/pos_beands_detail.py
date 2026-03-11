76496d98bfb2f08c27a8c150bf4d6caf2bde7f09
MohamedAliSmk <mohamedalismk@cgmail.com>
2026-03-11 14:15:20 +0200

    remove unuse child table pos brands detail

--- pos_next/pos_next/doctype/pos_beands_detail/pos_beands_detail.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, BrainWise and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class POSBeandsDetail(Document):
-	pass
